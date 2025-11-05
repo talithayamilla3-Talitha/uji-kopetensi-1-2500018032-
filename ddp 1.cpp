905bcceaf123eff56ff5dca26904e3f38dfb67c4
talithayamilla3-Talitha <talithayamilla3@gmail.com>
2025-11-05 11:57:47 +0700

    Update ddp 1.cpp

--- ddp 1.cpp
+++ ddp 1.cpp
@@ -1,20 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	int detik;
-	
-	cout << "masukkan bilangan lebih dari dari 1(untuk satuan detik) =";
-	cin >> detik;
-	
-	long long jam = detik / 3600;
-	long long sisa_detik = detik % 3600;
-	long long menit = sisa_detik / 60;
-	long long detik_akhir = sisa_detik % 60;
-	
-	cout << endl;
-	cout << "hasil konvesi :"<<endl; 
-	cout << detik << " " << "detik =" << jam << " " << "jam, " << menit << " " << "menit, " << " " << detik_akhir << "detik";
-		
-	
+int main() {
+	int jam, menit, sisa;
+    long dtk;
+    
+	cout << "+===========================================+" << endl;
+	cout << "|               KONVERSI WAKTU              |" << endl;
+	cout << "+===========================================+" << endl;
+    cout << "Masukkannlah bilangan (dalam satuan detik): ";
+    cin >> dtk;
+
+    jam = dtk/ 3600;
+    sisa = dtk % 3600;
+    menit = sisa / 60;
+    sisa = sisa % 60;
+
+    cout << endl;
+    cout << "|              Hasil Konversi:              |" <<endl; 
+    cout << dtk << " detik = " << jam << " Jam, " << menit 
+         << " Menit, " << sisa << " Detik." << endl;
+    cout << "*********************************************"<<endl;
+
+    return 0;
 }
